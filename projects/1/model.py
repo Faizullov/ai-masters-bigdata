cf6c18974876c94f6674dfb720cff3ee55393c3a
Faizullov <a.f.40@bk.ru>
2023-03-14 22:44:39 +0000

    Add all

--- projects/1/model.py
+++ projects/1/model.py
@@ -42,7 +42,7 @@ categorical_transformer = Pipeline(steps=[
 preprocessor = ColumnTransformer(
     transformers=[
         ('num', numeric_transformer, numeric_features),
-        # ('cat', categorical_transformer, categorical_features)
+        ('cat', categorical_transformer, categorical_features)
     ]
 )
 
--- projects/1/predict.py
+++ projects/1/predict.py
@@ -23,12 +23,15 @@ model = load("1.joblib")
 #num_reviews,CLEANLINESS,ROOM,SERVICE,LOCATION,VALUE,COMFORT,overall_ratingsource""".replace("\n",'').split(",")
 
 #read and infere
+fields.remove('label')
 read_opts=dict(
         sep='\t', names=fields, index_col=False, header=None,
         iterator=True, chunksize=100
 )
 
 for df in pd.read_csv(sys.stdin, **read_opts):
-    pred = model.predict(df)
-    out = zip(df.doc_id, pred)
-    print("\n".join(["{0},{1}".format(*i) for i in out]))
+    pred = model.predict_proba(df)[:, 1]
+#     pred = model.predict(df)
+    out = zip(df.id, pred)
+    print("\n".join(["{0}\t{1}".format(*i) for i in out]))
+    
