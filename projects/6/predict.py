e48726b13c99209d04d96b90733fcd642095a9c2
Faizullov <a.f.40@bk.ru>
2023-05-03 17:29:05 +0000

    HW6 changed

--- projects/6/predict.py
+++ projects/6/predict.py
@@ -22,11 +22,19 @@ PYSPARK_HOME = os.path.join(SPARK_HOME, "python/lib")
 sys.path.insert(0, os.path.join(PYSPARK_HOME, "py4j-0.10.9.3-src.zip"))
 sys.path.insert(0, os.path.join(PYSPARK_HOME, "pyspark.zip"))
 
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+
+conf = SparkConf()
+
+spark = SparkSession.builder.config(conf=conf).appName("Spark SQL").getOrCreate()
+
 from sklearn.feature_extraction.text import HashingVectorizer
 from sklearn.pipeline import Pipeline
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LogisticRegression
 import pandas as pd
+from joblib import load
 
 df = pd.read_json(args['test_in']).fillna( {"reviewText": "missingreview"})
 
